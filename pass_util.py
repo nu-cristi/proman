20d5a3ef1a93a85379fb39f7cab13c6600a54581
victorpuiu <puiu.danut@gmail.com>
2022-09-08 09:33:05 +0300

    44% done

---
+++ pass_util.py
@@ -0,0 +1,12 @@
+import bcrypt
+
+
+def hash_password(original_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(original_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(original_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(original_password.encode('utf-8'), hashed_bytes_password)
